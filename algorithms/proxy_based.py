ece1d907c09f48e39d0aa3c2af410ea55da68872
lorenzobloise <l.bloise@outlook.it>
2024-08-31 22:37:41 +0200

    Added SimPath algorithm

--- algorithms/proxy_based.py
+++ algorithms/proxy_based.py
@@ -3,10 +3,15 @@ from heapdict import heapdict
 import numpy as np
 from tqdm import tqdm
 import im
+import diffusion_models
+
+"""
+Influence ranking proxy algorithms
+"""
 
 class ProxyBasedAlgorithm:
     """
-    Proxy-based algorithms for seed set selection in influence maximization problems use heuristic measures
+    Proxy-based algorithms for seed set selection use heuristic measures
     to identify influential nodes in a network. These algorithms do not rely on extensive simulations
     but instead use structural properties of the graph to make decisions.
     """
@@ -64,7 +69,7 @@ class DegDis(ProxyBasedAlgorithm):
             dd[u] = -d[u]
             t[u] = 0
         # Add vertices to seed set
-        for _ in tqdm(range(self.budget), desc="DegDis",position=1, leave=True):
+        for _ in tqdm(range(self.budget), desc="DegDis", position=1, leave=True):
             u, _ = dd.popitem()
             seed_set.append(u)
             for v in self.graph[u]:
@@ -173,4 +178,142 @@ class Group_PR(ProxyBasedAlgorithm):
             progress_bar.update(1)
             influencee.remove(s_max)
             delta_dict[s_max] = 0
+        return seed_set
+
+
+"""
+Diffusion model reduction proxy algorithms
+"""
+
+
+class SimPath(ProxyBasedAlgorithm):
+    """
+    Can only be used under Linear Threshold diffusion model.
+    Paper: Goyal et al. - "SimPath: An Efficient Algorithm for Influence Maximization under the Linear Threshold Model"
+    """
+
+    name = 'simpath'
+    required_diff_model = diffusion_models.LinearThreshold()
+
+    class Node(object):
+
+        def __init__(self, node, spd=0, spd_induced=0, flag=False):
+            self.node = node
+            self.spd = 0
+            self.spd_induced = 0
+            self.flag=flag
+
+    # TODO
+    def __init__(self, graph, agent, budget, diff_model, r):
+        super().__init__(graph, agent, budget, diff_model, r)
+        if diff_model.name != 'lt':
+            raise ValueError(f"{self.__class__.__name__} can only be executed under "
+                             f"{self.required_diff_model.__class__.__name__} diffusion model "
+                             f"(set the argument at '{self.required_diff_model.name}')")
+        self.lookahead = 5
+        self.eta = 0.001
+
+    def simpath_spread(self, seed, U):
+        spd, spd_induced = 0, 0
+        for u in seed:
+            if self.graph.out_degree(u) > 0:
+                spd_new, spd_induced_new = self.backtrack(u, (set(self.graph.nodes) - set(seed)).union({u}), U)
+                spd += spd_new
+                spd_induced += spd_induced_new
+        return spd, spd_induced
+
+    def backtrack(self, u, W, U):
+        Q = [u]
+        spd, spd_induced, pp, D = 1, 1, 1, {}
+        while len(Q) > 0:
+            Q, D, spd, spd_induced, pp = self.forward(Q, D, spd, spd_induced, pp, W, U)
+            u = Q.pop()
+            del D[u]
+            if len(Q) == 0:
+                break
+            v = Q[-1]
+            pp = pp / self.graph.edges[v, u]['p']
+        return spd, spd_induced
+
+    def exists_y(self, x, Q, D, W):
+        """
+        As specified in the paper, this function returns a node y that is in the out-neighborhood of x
+        and not in Q and not in D[x] but it is in W
+        """
+        out_neighbors = {v for (x, v) in self.graph.out_edges(x)}
+        for y in out_neighbors:
+            if y not in Q and y not in D[x] and y in W:
+                return True, y
+        return False, None
+
+    def forward(self, Q, D, spd, spd_induced, pp, W, U):
+        x = Q[-1]
+        if x not in list(D.keys()):
+            D[x] = []
+        exists_y, y = self.exists_y(x, Q, D, W)
+        while exists_y:
+            if pp * self.graph.edges[x, y]['p'] < self.eta:
+                if x not in list(D.keys()):
+                    D[x] = []
+                D[x].append(y)
+            else:
+                Q.append(y)
+                pp = pp * self.graph.edges[x, y]['p']
+                spd += pp
+                if x not in list(D.keys()):
+                    D[x] = []
+                D[x].append(y)
+                x = Q[-1]
+                if x not in list(D.keys()):
+                    D[x] = []
+                for v in U:
+                    if v not in Q:
+                        spd_induced += pp
+            exists_y, y = self.exists_y(x, Q, D, W)
+        return Q, D, spd, spd_induced, pp
+
+    def __get_node_from_queue__(self, queue, node):
+        for u in queue:
+            if u.node == node:
+                return u
+        return None
+
+    def run(self):
+        import networkx.algorithms.approximation.vertex_cover as vc
+        # Compute the vertex cover of input graph
+        C = vc.min_weighted_vertex_cover(self.graph.to_undirected(as_view=True))
+        queue = heapdict()  # Priority queue based on the spread value
+        for u in C:
+            in_neighbor = {v for (v, u) in self.graph.in_edges(u)}
+            U = set((set(self.graph.nodes) - C).intersection(in_neighbor))
+            spd, spd_induced = self.simpath_spread([u], U)
+            node_data = self.Node(u, spd, spd_induced)
+            # Add node to the queue
+            queue[node_data] = -node_data.spd
+        for v in set(self.graph.nodes) - C:
+            # Compute the spread value of v as specified by Theorem 2
+            spread_v = 1 + sum([self.graph.edges[v,u]['p'] * self.__get_node_from_queue__(queue, u).spd_induced for (v, u) in self.graph.out_edges(v)])
+            queue[self.Node(v, spread_v)] = -spread_v # TODO: check
+        seed_set = []
+        spread = 0
+        while len(seed_set) < self.budget:
+            U = []
+            for _ in range(self.lookahead):
+                curr_node_data, _ = queue.popitem()
+                U.append(curr_node_data.node)
+                queue[curr_node_data] = -curr_node_data.spd
+            _, spd_induced_seed = self.simpath_spread(seed_set, U)
+            for x in U:
+                node_data_x = self.__get_node_from_queue__(queue, x)
+                if node_data_x.flag:
+                    seed_set.append(x)
+                    spread += node_data_x.spd
+                    del queue[node_data_x]
+                    break
+                _, spd_induced_seed_x = self.backtrack(x, set(self.graph.nodes)-set(seed_set), [])
+                spread_seed_x = spd_induced_seed_x + spd_induced_seed
+                marginal_gain_x = spread_seed_x - spread
+                node_data_x.flag = True
+                node_data_x.spd = marginal_gain_x
+                queue[node_data_x] = -node_data_x.spd
         return seed_set
--- algorithms/simulation_based.py
+++ algorithms/simulation_based.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 from heapdict import heapdict
 import networkx as nx
 import im
+import math
 
 class SimulationBasedAlgorithm:
     """
@@ -231,7 +232,7 @@ class CGA(SimulationBasedAlgorithm):
         sim_graph = self.graph.copy()
         seed_set = []
         # Detect communities
-        C = nx.community.louvain_communities(sim_graph)
+        C = nx.community.louvain_communities(sim_graph, weight='p')
         M = len(C)
         seed_communities = {i: [] for i in range(M)}
         R = {}
@@ -256,4 +257,83 @@ class CGA(SimulationBasedAlgorithm):
             v_max = max(marg_gains, key=marg_gains.get)
             seed_communities[j-1].append(v_max)
             seed_set.append(v_max)
+        return seed_set
+
+# TODO: check
+class CGINA(SimulationBasedAlgorithm):
+    """
+    Paper: Jiaguo LV et al. - A New Community-based Algorithm for Influence Maximization in Social Network
+    """
+
+    name = 'cgina'
+
+    def __init__(self, graph, agent, budget, diff_model, r=10000):
+        super().__init__(graph, agent, budget, diff_model, r)
+
+    def __get_edges_across_different_communities__(self, graph):
+        result = []
+        for (u, v, attr) in graph.edges(data=True):
+            if graph.nodes[u]['community'] != graph.nodes[v]['community']:
+                result.append((u, v, attr))
+        return result
+
+    def __get_p_of_edges_inside_community__(self, graph, community):
+        community_graph = graph.subgraph(community)
+        return [attr['p'] for (u, v, attr) in list(community_graph.edges(data=True))]
+
+    def __community_contribution__(self, graph, community):
+        maximal_n_of_edges = len(community) * (len(community) - 1)
+        wd = sum(self.__get_p_of_edges_inside_community__(graph, community)) / maximal_n_of_edges if maximal_n_of_edges != 0 else 0
+        return wd
+
+    def __shapley__(self, graph):
+        """
+        Calculate the Shapley value
+            :param graph: The subgraph representing a community C_i.
+            :return: The Shapley value of the community C_i.
+        """
+        communities = nx.community.louvain_communities(graph, weight='p')
+        return sum([self.__community_contribution__(graph, c)*len(graph.nodes) for c in communities])
+
+    def run(self):
+        l = 0.2 # Proportion of bridge nodes in key nodes, chosen empirically as shown in the paper
+        sim_graph = self.graph.copy()
+        seed_set = []
+        # Detect communities
+        communities = nx.community.louvain_communities(sim_graph, weight='p', resolution=0.3)
+        if len(communities) < self.budget:
+            raise ValueError(f"Budget {self.budget} is less than the number of communities found ({len(communities)}). Set a smaller value for the budget.")
+        # Preprocessing
+        for i in range(len(communities)):
+            for j in list(communities[i]):
+                sim_graph.nodes[j]['community'] = i
+                sim_graph.nodes[j]['bweight'] = 0
+        shapley_values = {i: self.__shapley__(self.graph.subgraph(list(communities[i]))) for i in range(len(communities))} # Shapley value for every community
+        out_nodes = {i: 0 for i in range(len(communities))} # Number of out nodes for every community
+        key_nodes = {} # Number of key nodes for every community
+        ssv = sum(list(shapley_values.values())) # Sum of Shapley values for all communities
+        for i in range(len(communities)):
+            key_nodes[i] = math.floor(shapley_values[i]/ssv)
+            print(key_nodes[i])
+        eb = self.__get_edges_across_different_communities__(sim_graph)
+        for (u, v, attr) in eb:
+            out_nodes[sim_graph.nodes[u]['community']] += 1
+        for (u, v, attr) in eb:
+            sim_graph.nodes[u]['bweight'] += attr['p']*shapley_values[sim_graph.nodes[v]['community']]
+        influential_nodes = {} # Number of influential nodes in each community
+        for i in range(len(communities)):
+            if out_nodes[i] >= key_nodes[i]*l:
+                # Choose the top key_nodes[c]*l nodes with the maximum bweight into topknodes
+                influential_nodes[i] = math.floor(key_nodes[i] - key_nodes[i]*l) # key_nodes[c]*l is the number of bridge nodes in the community c
+            else:
+                # Choose all out nodes into topknodes
+                influential_nodes[i] = key_nodes[i] - out_nodes[i]
+        while len(seed_set) < self.budget:
+            marginal_gains = {}
+            for i in range(len(communities)):
+                for j in range(influential_nodes[i]):
+                    marginal_gains[j] = im.simulation_delta(sim_graph, self.diff_model, self.agent, seed_set+[j], seed_set)
+            v_max = sorted(marginal_gains.items(), key=lambda item: item[1], reverse=True)[0][0]
+            seed_set.append(v_max)
+            communities.remove(communities[sim_graph.nodes[v_max]['community']])
         return seed_set
