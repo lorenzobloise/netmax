6b483f4ac0f576f21ee3384767302f2976adb4ba
lorenzobloise <l.bloise@outlook.it>
2024-11-27 21:12:41 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
 from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+__author__ = 'Lorenzo Bloise, Carmelo Gugliotta'
+__license__ = 'MIT License'
+__email__ = 'l.bloise@outlook.it - carmelo.gugliotta00@gmail.com'
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='netmax',
     packages=find_packages(include=['netmax', 'netmax.*']),
     version='0.1.0',
     description='Library for the problem of Influence Maximization in Social Networks',
-    long_description='NetMax is a python framework that provides the implementation of several algorithms for the problem of Influence Maximization in Social Networks. It also addresses the problem of Competitive Influence Maximization as an extensive-form strategic game setting.',
-    author='Lorenzo Bloise, Carmelo Gugliotta',
-    author_email='l.bloise@outlook.it',
+    long_description=long_description,
+    author=__author__,
+    author_email=__email__,
+    license=__license__,
     install_requires=['networkx==3.3', 'numpy', 'tqdm', 'heapdict'],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        f'License :: OSI Approved :: {__license__}',
         'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Network Analysis'
     ],
-    python_requires='>=3.12'
+    keywords='influence-maximization network-analysis simulator complex-networks',
+    python_requires='>=3.12',
 )
